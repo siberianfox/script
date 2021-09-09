4563585c5161a1608b4afbad7a4f9a2e9601861c
John Lin <shaojun.lin@delonghigroup.com>
2021-09-09 15:39:57 +0800

    Only move mp3 file

--- sortfile.py
+++ sortfile.py
@@ -7,23 +7,31 @@ print(cur)
 #Move biggest size mp3 file from directory to root directory 
 def move():
     for root, dirs, files in os.walk(cur):
-        files = list(filter(lambda x: os.path.splitext(x)[1] == '.mp3', files))
-        if len(dirs) == 0 and len(files) != 0:
-            size         = 0
-            target_abs   = ""
-            target_base  = ""
-            root_base    = os.path.basename(root)
+        
+        #Only folder that just contain file
+        if len(dirs) != 0 or len(files) == 0:
+            continue
+        
+        mp3 = list(filter(lambda x: os.path.splitext(x)[1] == '.mp3', files))
+        
+        root_base    = os.path.basename(root)
+        
+        size      = 0
+        mp3_abs   = ""
+        mp3_base  = ""
 
-            for f in files:
-                f_abs = os.path.join(root, f) # absolute file path
-                s = os.stat(f_abs).st_size
-                if  s >= size:
-                    size = s
-                    target_abs  = f_abs
-                    target_base = f
+        for f in mp3:
+            f_abs = os.path.join(root, f) # absolute file path
+            if  "pb" in f:
+                mp3_abs  = f_abs
+                mp3_base = f
+                size = os.stat(f_abs).st_size
+                break
 
-            print("moving file %50s to root, size is %d" % (target_base, size))
-            shutil.copy(target_abs, cur)
-            os.rename(os.path.join(cur, target_base), os.path.join(cur, root_base + ".mp3"))
+        print("moving file %50s to root, size is %d" % (mp3_base, size))
+        shutil.copy(mp3_abs, cur)
+        os.rename(os.path.join(cur, mp3_base), os.path.join(cur, root_base + ".mp3"))
 
+
+        
 move()
